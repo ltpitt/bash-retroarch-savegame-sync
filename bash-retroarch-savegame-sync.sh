d9d8efabcf443ee69c2b6a7d4d7e810981bf778d
ltpitt <d.nastri@gmail.com>
2021-06-10 08:47:48 +0100

    feat: Added helper functions to reduce duplicated code

--- bash-retroarch-savegame-sync.sh
+++ bash-retroarch-savegame-sync.sh
@@ -8,146 +8,150 @@
 
 
 ###
-# Requirements check
-###
-is_command_installed () {
-    if ! command -v $1 &> /dev/null
-    then
-        echo "${1} command is not available, please install it before running this script."
-        exit
-    fi
-}
-
-is_command_installed rsync
-is_command_installed lftp
-
-
-###
 # Customize the following variables according to your setup
 ###
-declare -A CONSOLES=(
-                     ["/home/pi"]="local"
+readonly -A CONSOLES=(
+#                     ["/home/pi"]="local"
                      ["pi@retropie:/home/pi"]="pitendo"
-                     ["ftp://nintendowii/sd/retroarch"]="nintendowii"
-                     ["ftp://psvita:1337/ux0:/data/retroarch"]="psvita"
+#                     ["ftp://nintendowii/sd/retroarch"]="nintendowii"
+#                     ["ftp://psvita:1337/ux0:/data/retroarch"]="psvita"
                     )
 
-START=`date +%s`
-BACKUP="/home/pi/Backup/savegames/"
-MAX_NUMBER_OF_BACKUPS_KEPT=15
-TMP="/tmp"
+readonly START=`date +%s`
+readonly BACKUP="/home/pi/Backup/savegames/"
+readonly MAX_NUMBER_OF_BACKUPS_KEPT=15
+readonly TMP="/tmp"
 
 ###
 # Script logic, no need to change anything below for setup
 ###
+check_requirements () {
+    requirements=( rsync lftp )
+    for requirement in "${requirements[@]}"
+    do
+        if ! command -v $requirement &> /dev/null
+        then
+            echo "${requirement} command is not available, please install it before running this script."
+            exit
+        fi
+    done
+}
+
+
+log_status () {
+    local status=$1
+    local source=$2
+    local target=$3
+    echo
+    echo "*************************************************************************"
+    echo "Status: ${status}"
+    echo "Source: ${source}"
+    echo "Target: ${target}"
+    echo "*************************************************************************"
+    echo
+}
+
+
+clean_up () {
+    rm -rf $TMP/savefiles/*.lftp-pget-status
+    rm -rf $TMP/savestates/*.lftp-pget-status
+    rm -Rf $TMP/savefiles || echo "Could not delete ${TMP}/savefiles"
+    rm -Rf $TMP/savestates || echo "Could not delete ${TMP}/savestates"
+    ITEMS_IN_BACKUP_FOLDER=$(ls $BACKUP  | wc -l)
+    if [ "$ITEMS_IN_BACKUP_FOLDER" -gt $MAX_NUMBER_OF_BACKUPS_KEPT ]; then
+        echo "Deleting oldest backup"
+        cd $BACKUP
+        ls $BACKUP -A1t | tail -n -1 | xargs rm -R
+    fi
+}
+
+
+check_requirements
 NOW=`date +"%Y%m%d%H%M"`
 
-rm -Rf $TMP/savefiles || echo "Could not delete ${TMP}/savefiles"
-rm -Rf $TMP/savestates || echo "Could not delete ${TMP}/savestates"
-mkdir -p $TMP/savefiles
-mkdir -p $TMP/savestates
 
+###
+# Getting existing savefiles and savegames for every available device
+###
 for CONSOLE in "${!CONSOLES[@]}";
     do
         mkdir -p $BACKUP$NOW/${CONSOLES[$CONSOLE]}/savefiles
         mkdir -p $BACKUP$NOW/${CONSOLES[$CONSOLE]}/savestates
         if [[ $CONSOLE == "ftp"* ]]; then
-            echo 
-            echo "*************************************************************************"
-            echo "Getting savefiles and savestates data for ${CONSOLES[$CONSOLE]}"
-            echo "Source 1: ${CONSOLE}/savefiles"
-            echo "Target 1: ${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savefiles"
-            echo "Source 2: ${CONSOLE}/savestates"
-            echo "Target 2: ${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savestates"
-            echo "*************************************************************************"
-            echo
             read ip < <(echo $CONSOLE | grep -o '[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+')
-            read path < <(echo $CONSOLE | awk -F $ip '{print $2}')
-            ftp_command="set ftp:passive-mode off; mirror --use-pget-n=10 ${CONSOLE}/savefiles/ ${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savefiles/; mirror --use-pget-n=10 ${CONSOLE}/savestates/ $BACKUP$NOW/${CONSOLES[$CONSOLE]}/savestates; bye"
+            status="Getting savefiles data and backing it up locally for ${CONSOLES[$CONSOLE]}"
+            source="${CONSOLE}/savefiles/"
+            target="${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savefiles/"
+            log_status "$status" "$source" "$target"
+            ftp_command="set ftp:passive-mode off; mirror --use-pget-n=10 ${CONSOLE}/savefiles/ ${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savefiles/; bye"
+            status="Getting savestates data and backing it up locally for ${CONSOLES[$CONSOLE]}"
+            source="${CONSOLE}/savestates/"
+            target="${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savestates"
+            log_status "$status" "$source" "$target"
+            ftp_command="set ftp:passive-mode off; mirror --use-pget-n=10 ${CONSOLE}/savestates/ $BACKUP$NOW/${CONSOLES[$CONSOLE]}/savestates; bye"
             lftp -e "${ftp_command}" $ip
         else
-            echo 
-            echo "*************************************************************************"
-            echo "Getting savefiles data for ${CONSOLES[$CONSOLE]}"
-            echo "Source: ${CONSOLE}/savefiles"
-            echo "Target: ${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savefiles"
-            echo "*************************************************************************"
-            echo
+            status="Getting savefiles data and backing it up locally for ${CONSOLES[$CONSOLE]}"
+            source="${CONSOLE}/savefiles/"
+            target="${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savefiles/"
+            log_status "$status" "$source" "$target"
             rsync -actzp $CONSOLE/savefiles $BACKUP$NOW/${CONSOLES[$CONSOLE]}
-            echo 
-            echo "*************************************************************************"
-            echo "Getting savestates data for ${CONSOLES[$CONSOLE]}"
-            echo "Source: ${CONSOLE}/savestates"
-            echo "Target: ${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savestates"
-            echo "*************************************************************************"
-            echo
+            status="Getting savestates data and backing it up locally for ${CONSOLES[$CONSOLE]}"
+            source="${CONSOLE}/savestates/"
+            target="${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savestates"
+            log_status "$status" "$source" "$target"
             rsync -actzp $CONSOLE/savestates $BACKUP$NOW/${CONSOLES[$CONSOLE]}
         fi
-        echo 
-        echo "*************************************************************************"
-        echo "Putting savefiles data for ${CONSOLES[$CONSOLE]} in /tmp"
-        echo "Source: ${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savefiles"
-        echo "Target: ${TMP}/savefiles"
-        echo "*************************************************************************"
-        echo
+        status="Putting savefiles data (in order to keeping the most recent ones from any device) for ${CONSOLES[$CONSOLE]} in /tmp"
+        source="${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savefiles"
+        target="${TMP}/savefiles"
+        log_status "$status" "$source" "$target"
         rsync -actzp --update $BACKUP$NOW/${CONSOLES[$CONSOLE]}/savefiles $TMP
-        echo 
-        echo "*************************************************************************"
-        echo "Putting savestates data for ${CONSOLES[$CONSOLE]} in /tmp"
-        echo "Source: ${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savestates"
-        echo "Target: ${TMP}/savestates"
-        echo "*************************************************************************"
-        echo
+        status="Putting savestates data (in order to keeping the most recent ones from any device) for ${CONSOLES[$CONSOLE]} in /tmp"
+        source="${BACKUP}${NOW}/${CONSOLES[$CONSOLE]}/savestates"
+        target="${TMP}/savestates"
+        log_status "$status" "$source" "$target"
         rsync -actzp --update $BACKUP$NOW/${CONSOLES[$CONSOLE]}/savestates $TMP
     done
 
-rm -rf $TMP/savefiles/*.lftp-pget-status
-rm -rf $TMP/savestates/*.lftp-pget-status
 
+###
+# Sending most recent savefiles and savegames to every available device
+###
+mkdir -p $TMP/savefiles
+mkdir -p $TMP/savestates
 for CONSOLE in "${!CONSOLES[@]}";
     do
         if [[ $CONSOLE == "ftp"* ]]; then
-            echo
-            echo "*************************************************************************"
-            echo "Sending updated savefiles and savestates data to ${CONSOLES[$CONSOLE]}"
-            echo "Source 1: ${TMP}/savefiles"
-            echo "Target 1: ${CONSOLE}/savefiles"
-            echo "Source 2: ${TMP}/savestates"
-            echo "Target 2: ${CONSOLE}/savestates"
-            echo "*************************************************************************"
-            echo
             read ip < <(echo $CONSOLE | grep -o '[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+')
-            read path < <(echo $CONSOLE | awk -F $ip '{print $2}')
-            ftp_command="set ftp:passive-mode off; mirror -R ${TMP}/savefiles ${CONSOLE}/savefiles; mirror -R ${TMP}/savestates ${CONSOLE}/savestates; bye"
+            status="Sending updated savefiles data to ${CONSOLES[$CONSOLE]}"
+            source="${TMP}/savefiles"
+            target="${CONSOLE}/savefiles"
+            log_status "$status" "$source" "$target"
+            ftp_command="set ftp:passive-mode off; mirror -R ${TMP}/savefiles ${CONSOLE}/savefiles; bye"
+            status="Sending updated savestates data to ${CONSOLES[$CONSOLE]}"
+            source="${TMP}/savestates"
+            target="${CONSOLE}/savestates"
+            log_status "$status" "$source" "$target"
+            ftp_command="set ftp:passive-mode off; mirror -R ${TMP}/savestates ${CONSOLE}/savestates; bye"
             lftp -e "${ftp_command}" $ip
         else
-            echo 
-            echo "*************************************************************************"
-            echo "Sending updated savefiles data to ${CONSOLES[$CONSOLE]}"
-            echo "Source: ${TMP}/savefiles"
-            echo "Target: ${CONSOLE}/savefiles"
-            echo "*************************************************************************"
-            echo
+            status="Sending updated savefiles data to ${CONSOLES[$CONSOLE]}"
+            source="${TMP}/savefiles"
+            target="${CONSOLE}/savefiles"
+            log_status "$status" "$source" "$target"
             rsync -actzp $TMP/savefiles $CONSOLE
-            echo 
-            echo "*************************************************************************"
-            echo "Sending updated savestates data to ${CONSOLES[$CONSOLE]}"
-            echo "Source: ${TMP}/savestates"
-            echo "Target: ${CONSOLE}/savestates"
-            echo "*************************************************************************"
-            echo
+            status="Sending updated savestates data to ${CONSOLES[$CONSOLE]}"
+            source="${TMP}/savestates"
+            target="${CONSOLE}/savestates"
+            log_status "$status" "$source" "$target"
             rsync -actzp $TMP/savestates $CONSOLE
         fi
     done
 
 
-ITEMS_IN_BACKUP_FOLDER=$(ls $BACKUP  | wc -l)
-if [ "$ITEMS_IN_BACKUP_FOLDER" -gt $MAX_NUMBER_OF_BACKUPS_KEPT ]; then
-    echo "Deleting oldest backup"
-    cd $BACKUP
-    ls $BACKUP -A1t | tail -n -1 | xargs rm -R
-fi
+clean_up
 
 END=`date +%s`
 RUNTIME=$((END-START))
-echo $RUNTIME
+echo "The full run took ${RUNTIME} seconds."
